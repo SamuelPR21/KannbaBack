de7bc7eee10a54032e24ac0217698420be07ccdf
Samuel A. Perdomo Fajardo <u20222208795@usco.edu.co>
2025-10-26 18:47:40 -0500

    modificacion de rutas de user.controller.ts

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,17 +5,17 @@ import { CreateUserDto } from "./DTO/create-user.dto";
 import { LoginUserDto } from "./DTO/login-user.dto";
 import { UpdateUserDto } from "./DTO/update-user.dto";
 
-@Controller('users')
+@Controller('/users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Post('register')
+  @Post('/register')
   async register(@Body() dto: CreateUserDto, @Res() res: Response) {
     const user = await this.usersService.register(dto);
     return res.json({ message: 'Usuario registrado correctamente', user });
   }
 
-  @Post('login')
+  @Post('/login')
   async login(@Body() dto: LoginUserDto, @Res() res: Response) {
     const { token, user} = await this.usersService.login(dto);
     res.setHeader("Authorization", `Bearer ${token}`);
@@ -23,19 +23,19 @@ export class UsersController {
     return res.json({message: "Inicio de sesión exitoso", user});
   }
 
-  @Get(':id')
+  @Get('/:id')
   async getUser(@Param('id') id: number) {
     const user = await this.usersService.findOne(id);
     return { user };
   }
 
-  @Put(':id')
+  @Put('/:id')
   async updateUser(@Param('id') id: number, @Body() dto: UpdateUserDto) {
     const updated = await this.usersService.update(id, dto);
     return { message: 'Usuario actualizado', updated };
   }
 
-  @Delete(':id')
+  @Delete('/:id')
   async deleteUser(@Param('id') id: number) {
     await this.usersService.delete(id);
     return { message: 'Usuario eliminado correctamente' };
