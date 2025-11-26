0855352e4a756e66e379d86d99cb653c0f5dff78
SamuelPR21 <u20222208795@usco.edu.co>
2025-11-26 17:22:55 -0500

    Integracion de mascota para el front

--- src/pet/controller/pet.controller.ts
+++ src/pet/controller/pet.controller.ts
@@ -1,19 +1,23 @@
 import { Controller, Get, Param, Post } from "@nestjs/common";
 import { PetService } from "../services/pet.service";
+import { UserId } from 'src/common/decorator/user-id.decorator';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { UseGuards } from '@nestjs/common';
 
 @Controller('/pet')
+@UseGuards(JwtAuthGuard)
 export class PetController {
     constructor(
         private readonly petService: PetService
     ){}
 
-    @Get('/state/:userId')
-    async getPetState(@Param('userId') userId: number) {
+    @Get('/state')
+    async getPetState(@UserId() userId: number) {
         return this.petService.getState(userId);
     }
 
-    @Post('/:userId/feed')
-    async feedPet(@Param('userId') userId: number) {
+    @Post('/feed')
+    async feedPet(@UserId() userId: number) {
         return this.petService.feedPet(userId);
     }
 }
